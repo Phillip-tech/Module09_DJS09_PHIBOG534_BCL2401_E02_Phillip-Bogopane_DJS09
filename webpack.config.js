691a1946d1d7b7082dac31ee790c48198c88f122
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-10 18:20:58 +0200

    Updated files and code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
-module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
-  },
-  "entry": {
-    "index": "./index"
-  },
-  "resolve": {
-    "extensions": [
-      ".ts",
-      ".js",
-      ".json"
-    ]
-  },
-  "module": {
-    "rules": [
-      {
-        "use": {
-          "loader": "ts-loader"
+const path = require('path');
+
+  module.exports = {
+    entry: './index.ts',
+    mode: 'development',
+   devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
-        "exclude": /node_modules/,
-        "test": /\.ts$/
-      }
-    ]
-  }
-};
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: '[name].pack.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+  };
